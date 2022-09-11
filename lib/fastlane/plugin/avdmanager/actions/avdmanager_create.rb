df949791b01c73eab307de5e9d3c06e637ac9687
Blair Replogle <blair.replogle@gmail.com>
2022-09-11 17:32:33 -0600

    implement advmanager create

--- lib/fastlane/plugin/avdmanager/actions/avdmanager_create.rb
+++ lib/fastlane/plugin/avdmanager/actions/avdmanager_create.rb
@@ -3,15 +3,40 @@ module Fastlane
 	module Actions
 
 		module SharedValues
-			AVDMANAGER_CREATE_CUSTOM_VALUE = :AVDMANAGER_CREATE_CUSTOM_VALUE
+			AVDMANAGER_CREATE_NAME = :AVDMANAGER_CREATE_NAME
 		end
 
 		class AvdmanagerCreateAction < Action
 
 			def self.run(params)
-				sdk_path = params[:sdk_path]
+				FastlaneCore::PrintTable.print_values(
+					config: params,
+					title: 'ADV Manager Create Summary'
+				)
 
-				sh "#{sdk_path}/cmdline-tools/latest/bin/avdmanager list device"
+				sdk_path = params[:sdk_path]
+				name = params[:name]
+				package = params[:package]
+				device = params[:device]
+				size = params[:size]
+				avdmanager_path = Helper::AVDManager.avdmanager_path(params)
+
+				command = "#{avdmanager_path} create avd"
+				command << " -n '#{name}'"
+				command << " -k '#{package}'"
+				command << " -d '#{device}'"
+				command << " -c '#{size}'"
+				command << " -f"
+
+				FastlaneCore::CommandExecutor.execute(
+					command: command,
+					print_all: true,
+					print_command: true
+				)
+
+				lane_context[SharedValues::AVDMANAGER_CREATE_NAME] = name
+
+				name
 			end
 
 			#####################################################
@@ -19,49 +44,59 @@ module Fastlane
 			#####################################################
 
 			def self.description
-				"A short description with <= 80 characters of what this action does"
-			end
-
-			def self.details
-				# Optional:
-				# this is your chance to provide a more detailed description of this action
-				"You can use this action to do cool things..."
+				'Create a new AVD'
 			end
 
 			def self.available_options
-				# Define all options your action supports.
-
-				# Below a few examples
 				[
 					FastlaneCore::ConfigItem.new(
 						key: :sdk_path,
-						env_name: "AVDMANAGER_CREATE_SDK_PATH",
-						description: "Path to the Android sdk",
+						env_name: 'AVDMANAGER_CREATE_SDK_PATH',
+						description: 'Path to the Android sdk',
+						default_value: ENV['ANDROID_HOME'] || ENV['ANDROID_SDK_ROOT'] || ENV['ANDROID_SDK']
 					),
 					FastlaneCore::ConfigItem.new(
-						key: :development,
-						env_name: "FL_AVDMANAGER_CREATE_DEVELOPMENT",
-						description: "Create a development certificate instead of a distribution one",
-						is_string: false, # true: verifies the input is a string, false: every kind of value
-						default_value: false
+						key: :name,
+						env_name: 'AVDMANAGER_CREATE_NAME',
+						description: 'Name of the device',
+						type: String,
+						default_value: 'fastlane'
+					),
+					FastlaneCore::ConfigItem.new(
+						key: :package,
+						env_name: 'AVDMANAGER_CREATE_PACKAGE',
+						description: 'The selected system image of the emulator',
+						type: String,
+						default_value: 'system-images;android-25;google_apis;x86'
+					),
+					FastlaneCore::ConfigItem.new(
+						key: :device,
+						env_name: 'AVDMANAGER_CREATE_DEVICE',
+						description: 'The selected system image of the emulator',
+						type: String,
+						default_value: 'pixel'
+					),
+					FastlaneCore::ConfigItem.new(
+						key: :size,
+						env_name: 'AVDMANAGER_CREATE_SIZE',
+						description: 'The selected system image of the emulator',
+						type: String,
+						default_value: '4096M'
 					)
 				]
 			end
 
 			def self.output
-				# Define the shared values you are going to provide
-				# Example
 				[
-					['AVDMANAGER_CREATE_CUSTOM_VALUE', 'A description of what this value contains']
+					['AVDMANAGER_CREATE_NAME', 'The name of the AVD device that was created']
 				]
 			end
 
 			def self.return_value
-				# If your method provides a return value, you can describe here what it does
+				'The name of the AVD device that was created'
 			end
 
 			def self.authors
-				# So no one will ever forget your contribution to fastlane :) You are awesome btw!
 				["UpBra"]
 			end
 
--- lib/fastlane/plugin/avdmanager/helper/avdmanager_helper.rb
+++ lib/fastlane/plugin/avdmanager/helper/avdmanager_helper.rb
@@ -6,11 +6,20 @@ module Fastlane
 	module Helper
 
 		class AVDManager
-			# class methods that you define here become available in your action
-			# as `Helper::AVDManager.your_method`
-			#
-			def self.show_message
-				UI.message("Hello from the avdmanager plugin helper!")
+
+			def self.avdmanager_path(params)
+				sdk_path = params[:sdk_path]
+
+				unless File.exist?(sdk_path)
+					UI.user_error! "Andoid SDK does not exist at path: #{sdk_path}"
+				end
+
+				latest = "#{sdk_path}/cmdline-tools/latest/bin/avdmanager"
+				if File.exist?(latest)
+					return latest
+				end
+
+				UI.user_error! "Unable to locate avdmanager executable!"
 			end
 		end
 	end
