f940c4077a6c8fa360bed9329f4f2bcbfdc808e0
Blair Replogle <blair.replogle@gmail.com>
2022-09-11 13:39:03 -0600

    [0000] Start.

---
+++ lib/fastlane/plugin/avdmanager/version.rb
@@ -0,0 +1,5 @@
+module Fastlane
+  module Avdmanager
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/avdmanager_action_spec.rb
@@ -0,0 +1,9 @@
+describe Fastlane::Actions::AvdmanagerAction do
+  describe '#run' do
+    it 'prints a message' do
+      expect(Fastlane::UI).to receive(:message).with("The avdmanager plugin is working!")
+
+      Fastlane::Actions::AvdmanagerAction.run(nil)
+    end
+  end
+end
