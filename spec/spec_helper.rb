9ac6f84fe20190ce37f82868e1bcf8b4d18ebd99
Blair Replogle <blair.replogle@gmail.com>
2022-09-15 13:36:52 -0600

    [0008] Change Plugin Name (#7)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,6 @@ module SpecHelper
 end
 
 require 'fastlane' # to import the Action super class
-require 'fastlane/plugin/avdmanager' # import the actual plugin
+require 'fastlane/plugin/androidsdk' # import the actual plugin
 
 Fastlane.load_actions # load other actions (in case your plugin calls other actions or shared values)
